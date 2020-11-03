0872fc816e3e6dba40cd8cf58f10804620ec80c0
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-03 22:59:40 +0700

    add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "production",
+  entry: './src/index.js',
+  mode: 'production',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
+    ],
   },
 };
